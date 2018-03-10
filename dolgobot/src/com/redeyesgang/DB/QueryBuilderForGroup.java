252716a412f05d4f461df823a3a81256b45f07ba
Unknown <andr.crus@gmail.com>
2018-03-10 17:16:33 +0300

    CreateQueryBuilderForGroups

---
+++ dolgobot/src/com/redeyesgang/DB/QueryBuilderForGroup.java
@@ -0,0 +1,16 @@
+package com.redeyesgang.DB;
+
+public class QueryBuilderForGroup {
+    public static String getCreateGroupQuery(String groupName) {
+        return  "CREATE TABLE "+groupName+"(member INT REFERENCES users(telegramID));";
+
+    }
+
+    public static String getSelectFromGroupQuery(String groupName) {
+        return "SELECT userID FROM " + groupName;
+    }
+
+    public static String getDeleteGroupQuery(String groupName) {
+        return "DROP TABLE " + groupName;
+    }
+}
