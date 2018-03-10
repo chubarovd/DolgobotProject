74d7737a7b4357aaabcc535e1283253c7c41392b
Unknown <andr.crus@gmail.com>
2018-03-10 17:57:25 +0300

    arch fixes

--- dolgobot/src/com/redeyesgang/DB/IValidateTransaction.java
+++
@@ -1,7 +0,0 @@
-package com.redeyesgang.DB;
-
-import java.sql.SQLException;
-
-public interface IValidateTransaction {
-    void validate(int transactID) throws SQLException, TransactionException;
-}
